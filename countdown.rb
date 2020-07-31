685fbdd3437b0a3291c8383595b077ffc66d3807
Shalom Kebede <shalomseleshi@gmail.com>
2020-07-31 00:27:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,7 +6,7 @@ def countdown(num)
     num -= 1
   end
   
-  return "HAPPY NEW YEAR!\n"
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(num)
@@ -16,5 +16,5 @@ def countdown_with_sleep(num)
     sleep 1
   end
   
-  return "HAPPY NEW YEAR!\n"
+  return "HAPPY NEW YEAR!"
 end
