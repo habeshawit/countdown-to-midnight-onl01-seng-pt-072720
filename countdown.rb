9f9f3fbb0460382ea99cb53904e58092b24f7ef4
Shalom Kebede <shalomseleshi@gmail.com>
2020-07-31 00:30:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 
 def countdown(num)
-  while num > 0
+  while num != 0
     puts "#{num} SECOND(S)!\n"
     num -= 1
   end
@@ -10,7 +10,7 @@ def countdown(num)
 end
 
 def countdown_with_sleep(num)
-  while num > 0
+  while num != 0
     puts "#{num} SECOND(S)!\n"
     num -= 1
     sleep 1
